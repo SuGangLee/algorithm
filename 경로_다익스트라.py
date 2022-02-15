1391e5b20468ee6f2587477bc0ecdad6d6724413
SuGangLee <dltnrud001103@naver.com>
2022-02-15 23:27:45 +0900

    20220215

---
+++ 경로_다익스트라.py
@@ -0,0 +1,43 @@
+#우선순위 큐 라이브러리 > 최소 힙 구조 사용- 값이 낮은 데이터를 먼저 삭제 하는 
+#튜플 데이터를 (거리,노드)로 큐에 삽입 
+import heapq
+import sys
+input = sys.stdin.readline
+INF = int(1e9) 
+
+
+n,m = map(int,input().split())#노드, 간선 개수 입력
+start = int(input()) # 시작노드
+# 각노드에 연결되어있는 노드에 대한 정보를 담는 리스트
+graph = [[] for i in range (n+1)]
+distance = [INF] * (n+1)
+
+# 모든 간선 정보 입력
+for _ in range(m):
+    a,b,c = map(int,input().split())
+    # a번 노드에서 b번 노드로 가는 비용이 c
+    graph[a].append(b,c)
+
+def dijkstra (start):
+    q=[]
+    heapq.heappush(q,(0,start)) 
+    distance[start] = 0
+    while q: #큐가 비어있지 않다면
+        dist,now = heapq.heappop(q) #가장 최단 거리가 짧은 노드에 대한 정보 꺼내기
+        # 처리된 적 있는 노드라면 무시
+        if distance[now] < dist :
+            continue
+        #현재 노드와 인접한 다른 노드들 확인 
+        for i in graph[now]:
+            cost = dist +i[1]
+            if cost < distance[i[0]]:
+                distance [i[0]] = cost
+                heapq.heappush(q,(cost,i[0]))
+
+dijkstra(start)
+
+for i in range(1,n+1):
+    if distance[i] == INF:
+        print('infinity')
+    else:
+        print (distance[i])
---
+++ 경로_플로이드워셜.py
@@ -0,0 +1,37 @@
+INF = int(1e9)
+#A->B노드를 이동할 떄 n번 노드를 거쳐서 이동하는 경로 중 비용 최솟값 찾기
+#A -> n -> B
+
+#플로이드 워셜 알고리즘 : n=1을 거쳐서 이동하는 경로 중 최솟값 찾기-> D23, D24, D32, D34, D42, D43 이 여섯가지 경우 중 하나. 
+#  D23 = min(D23/1을 거치지 않고 곧 바로 이동,D2->1 + D1->3/1거쳐 이동) 
+
+n,m = map(int,input().split()) # n=노드개수, m=간선
+graph = [[INF]*(n+1) for _ in range(n+1)]
+
+#자기자신 -> 자기자신으로 가는 비용은 0 
+for a in range(1,n+1):
+    for b in range(1,n+1):
+        if a==b:
+            graph[a][b] = 0
+
+# 각 간선에 대한 정보를 입력받아 그 값으로 초기화
+for _ in range(m):
+    #a->b로 가는 비용,c
+    a,b,c = map(int,input().split())
+    graph[a][b] =c
+
+#점화식에 따라 플로이드 워셜 알고리즘 수행
+for k in range(1,n+1):
+    for a in range(1,n+1):
+        for b in range(1,n+1):
+            graph[a][b] = min(graph[a][b],graph[a][k]+graph[k][b])
+
+#수행 결과 출력
+for a in range(1,n+1):
+    for b in range(1,n+1):
+        if graph[a][b] ==INF:
+            print ("&",end=' ')
+        else:
+            print(graph[a][b],end=" ")
+    print()
+    
